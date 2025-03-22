cd4ea72f4b64d47618cf1712f7d60e1a0f7dde18
AdityaYeole2211 <adityayeole1899@gmail.com>
2025-03-22 12:42:20 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path : './env'}) //works but code consistency issues
 
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : "./env"
+})
+connectDB() //call db-connection function here 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //generally iffe fucntions used -> executes as soon as index file loads
 // ; written before as generally gives probelms if previous line of code 
 //does not have ;
-
+const app = express()
 ;(async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        
+        //maybe the database is connected but our app is not bale to talk with it 
+        //for that this listenrner used 
+        app.on("error",  (error) => {
+            console.error("Errrr: ", error)
+            throw error
+        })
+        app.listen(process.env.PORT, () =>{
+            console.log(`App listening on port : ${process.env.PORT}`)
+        })
     
     } catch (error) {
         console.error(error)
         throw error    
     }
 })()
+*/
