555d3a609017dade6fdf0519c151ed25b95b06fe
AdityaYeole2211 <adityayeole1899@gmail.com>
2025-03-26 19:41:43 +0530

    routes and controller defined

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.json({limit : "16kb"})) // data from json
 app.use(express.urlencoded({limit : "16kb"}))//data from urls
 app.use(express.static("public")) //save certain files on server only (in public folder)
 app.use(cookieParser())
+
+//import routes 
+import userRouter from "./routes/user.routes.js"
+
+app.use('/api/v1/users', userRouter)
+//url will be http://localhost:8000/api/v1/users/register
 export {app}
